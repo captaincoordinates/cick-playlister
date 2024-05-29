a686dec382a2b248347ab3bc673c5ba91639d9a3
captaincoordinates <thomaschristian@gmail.com>
2024-05-28 21:45:35 -0700

    Initial working client script (no styling)

--- internal/client/cick-playlister-client.js
+++ internal/client/cick-playlister-client.js
@@ -47,8 +47,31 @@ window.cickPlaylisterClient = {
               }
             })
             .then(data => {
+              this._classifyTableRows();
               data.tracks.forEach(track => {
-                this._addTrack(track);
+                var trackHash = this._hashEntry(
+                  track.artist,
+                  track.track,
+                  track.album
+                );
+                var trackRowEls = document.querySelectorAll([
+                  "[",
+                  this._trackHashAttribute,
+                  "=\"",
+                  trackHash,
+                  "\"]"
+                ].join(""));
+                switch (trackRowEls.length) {
+                  case 0:
+                    this._fillRow(track);
+                    break;
+                  case 1:
+                    console.debug("skipping " + trackHash);
+                    break;
+                  default:
+                    console.log("unexpectedly found multiple rows with " + trackHash);
+                    this._reportError();
+                }
               });
             })
             .catch(err => {
@@ -76,17 +99,6 @@ window.cickPlaylisterClient = {
       },
     }
   },
-  _addTrack: function(track) {
-    /*
-      album: "Penny Penguin"
-      artist: "Raffi, Good Lovelies"
-      isNew: false
-      isSingle: true
-      track: "Penny Penguin"
-    */
-    // still need a way to ensure that tracks are not repeated, e.g. base64-encoding an identifier
-    console.log(track);
-  },
   _getAnchor: function() {
     return document.getElementById(this.anchorId);
   },
@@ -96,5 +108,86 @@ window.cickPlaylisterClient = {
   },
   _reportFeedback: function(message, inputId) {
     console.log(message);
-  }
+  },
+  _trackHashAttribute: "data-row-track-hash",
+  _trackHashEmptyValue: "empty",
+  _trackRowCounterAttribute: "data-row-counter",
+  _trackSingleValue: "Single",
+  _classifyTableRows: function() {
+    Array.from(document.getElementById("station-playlist-tracks-table").getElementsByTagName("tr")).forEach(rowEl => {
+      var artistInput = null;
+      var trackInput = null;
+      var albumInput = null;
+      var rowCounter = null;
+      var artistInputIdRegex = /^edit-tracks-(\d+)-artist$/
+      Array.from(rowEl.getElementsByTagName("input")).forEach(inputEl => {
+        match = inputEl.id.match(artistInputIdRegex);
+        if (!!match) {
+          rowCounter = match[1];
+          artistInput = this._getArtistInput(rowCounter);
+          trackInput = this._getTrackInput(rowCounter);
+          albumInput = this._getAlbumInput(rowCounter);
+        }
+      });
+      if (artistInput && trackInput && albumInput) {
+        var hashValue = this._trackHashEmptyValue;
+        if (artistInput.value && trackInput.value && albumInput.value) {
+          hashValue = this._hashEntry(
+            artistInput.value,
+            trackInput.value,
+            albumInput.value
+          );
+        }
+        rowEl.setAttribute(this._trackHashAttribute, hashValue);
+        rowEl.setAttribute(this._trackRowCounterAttribute, rowCounter);
+      }
+    });
+  },
+  _fillRow: function(track) {
+    var emptyRowEls = document.querySelectorAll([
+      "[",
+      this._trackHashAttribute,
+      "=\"",
+      this._trackHashEmptyValue,
+      "\"]"
+    ].join(""));
+    if (emptyRowEls.length === 0) {
+      this._reportFeedback("no rows available");
+      return;
+    }
+    var nextRow = emptyRowEls[0];
+    var rowCounter = parseInt(nextRow.getAttribute(this._trackRowCounterAttribute), 10);
+    this._getArtistInput(rowCounter).value = track.artist;
+    this._getTrackInput(rowCounter).value = track.track;
+    this._getAlbumInput(rowCounter).value = (track.isSingle ? this._trackSingleValue : track.album);
+    this._getIsNewInput(rowCounter).checked = track.isNew;
+    nextRow.setAttribute(this._trackHashAttribute, this._hashEntry(
+      track.artist,
+      track.track,
+      track.album
+    ));
+  },
+  _getArtistInput(rowCounter) {
+    return document.getElementById("edit-tracks-" + rowCounter + "-artist");
+  },
+  _getTrackInput(rowCounter) {
+    return document.getElementById("edit-tracks-" + rowCounter + "-title");
+  },
+  _getAlbumInput(rowCounter) {
+    return document.getElementById("edit-tracks-" + rowCounter + "-album");
+  },
+  _getIsNewInput(rowCounter) {
+    return document.getElementById("edit-tracks-" + rowCounter + "-newtrack");
+  },
+  _hashEntry: function(artist, title, album) {
+    return btoa(
+      encodeURIComponent(
+        JSON.stringify({
+          artist: artist,
+          title: title,
+          album: album
+        })
+      )
+    );
+  },
 };
