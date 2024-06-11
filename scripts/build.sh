e23fefbff72e50a4f1feafadc285fa39a68f219a
captaincoordinates <thomaschristian@gmail.com>
2024-06-10 23:09:08 -0700

    creating windows package

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,14 @@ pushd $(dirname $0)/..
 scripts/validate-openapi.sh
 scripts/build-client.sh
 
+output_dir=$(pwd)/dist/$(date +%Y-%m-%d)
+mkdir -p $output_dir
+output_path=$output_dir/click-playlister.exe
+
+cp bookmarklet.js $output_dir/
+
 pushd cmd/cick-playlister
-go build
+GOOS=windows GOARCH=amd64 go build -o $output_path cick-playlister.go
+if [ -f credentials.json ]; then
+    cp credentials.json $output_dir/
+fi
