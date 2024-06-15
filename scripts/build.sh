de4796ab30819d68a2c94d39b8d8ede610d4b731
captaincoordinates <thomaschristian@gmail.com>
2024-06-14 22:58:59 -0700

    dockerised distributable build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,14 +8,24 @@ pushd $(dirname $0)/..
 scripts/validate-openapi.sh
 scripts/build-client.sh
 
-output_dir=$(pwd)/dist/$(date +%Y-%m-%d)
-mkdir -p $output_dir
-output_path=$output_dir/click-playlister.exe
+date_tag=$(date +%Y-%m-%d)
+local_output_dir=$(pwd)/dist/$date_tag
+mkdir -p $local_output_dir
 
-cp bookmarklet.js $output_dir/
-
-pushd cmd/cick-playlister
-GOOS=windows GOARCH=amd64 go build -o $output_path cick-playlister.go
-if [ -f credentials.json ]; then
-    cp credentials.json $output_dir/
+cp bookmarklet.js $local_output_dir/
+if [ -f cmd/cick-playlister/credentials.json ]; then
+    cp cmd/cick-playlister/credentials.json $local_output_dir/
 fi
+
+image_name="captaincoordinates/cick-playlister-builder"
+
+docker build \
+    -t $image_name \
+    -f scripts/api/Dockerfile \
+    --build-arg date_tag=${date_tag} \
+    .
+
+docker run \
+    --rm \
+    -v $(pwd)/dist:/src/dist:rw \
+    $image_name
